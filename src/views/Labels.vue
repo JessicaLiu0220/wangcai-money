7a6551dd94de672eba0c986ec84e8d1c2dfde03b
Jessica <1330784336@qq.com>
2022-01-10 19:57:13 +0800

    更改labels的button样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,21 +1,22 @@
 <template>
   <Layout>
-    <div class="createTag-wrapper">
-      <Button class="createTag" @click="createTag">
-        <Icon name="add" />添加类别
-      </Button>
-      <div class="line"></div>
-    </div>
-    <div class="tags">
-      <router-link
-        class="tag"
-        v-for="tag in tags"
-        :key="tag.id"
-        :to="`/labels/edit/${tag.id}`"
-      >
-        <span>{{ tag.name }}</span>
-        <Icon name="right" />
-      </router-link>
+    <div class="labels">
+      <div class="tags">
+        <router-link
+          class="tag"
+          v-for="tag in tags"
+          :key="tag.id"
+          :to="`/labels/edit/${tag.id}`"
+        >
+          <span>{{ tag.name }}</span>
+          <Icon name="right" />
+        </router-link>
+      </div>
+      <div class="createTag-wrapper">
+        <Button class="createTag" @click="createTag">
+          <Icon name="add" />添加类别
+        </Button>
+      </div>
     </div>
   </Layout>
 </template>
@@ -26,9 +27,10 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Button from "@/components/Button.vue";
 
+tagListModel.fetch();
 @Component
 export default class Types extends Vue {
-  tags = window.tagList;
+  tags = tagListModel.data;
 
   createTag() {
     const name = window.prompt("请输入新的类别名：");
@@ -50,55 +52,49 @@ export default class Types extends Vue {
 * {
   color: $color-deep;
 }
-.tags {
-  font-size: 16px;
-  margin-left: 5px;
-
-  > .tag {
-    display: flex;
-    padding: 10px 15px 10px 5px;
-    border-bottom: 1px solid $color-shadow;
-    justify-content: space-between;
-    align-items: center;
-    > span {
-      height: 44px;
-      min-width: 44px;
-      background: $color-shadow;
-      border: 0.1px solid $color-deep;
-      border-radius: 10px;
-      text-align: center;
-      line-height: 44px;
-      padding: 0px 5px;
-    }
-    > .icon {
-      width: 20px;
-      height: 20px;
-    }
-  }
-}
-.createTag-wrapper {
-  width: 100%;
-  height: 64px;
-  text-align: 64px;
-  background: $color-highlight;
-  display: flex;
-  justify-content: center;
+.labels {
   position: relative;
-  > .createTag {
-    font-size: 24px;
-    background: $color-highlight;
-    border: none;
-    position: relative;
+  height: 100%;
+  > .tags {
+    font-size: 16px;
+    height: 86%;
+    overflow: auto;
+    > .tag {
+      display: flex;
+      padding: 10px 15px 10px 5px;
+      border-bottom: 1px solid $color-shadow;
+      justify-content: space-between;
+      align-items: center;
+      > span {
+        height: 44px;
+        min-width: 44px;
+        background: $color-highlight;
+        border-radius: 10px;
+        text-align: center;
+        line-height: 44px;
+        padding: 0px 5px;
+      }
+      > .icon {
+        width: 20px;
+        height: 20px;
+      }
+    }
   }
-  > .line {
-    content: "";
+  > .createTag-wrapper {
+    left: 50%;
+    bottom: 20px;
+    margin-left: -80px;
     position: absolute;
-    left: 0px;
-    bottom: 0px;
-    width: 100%;
-    height: 4px;
-    background-color: #555555;
+    text-align: center;
+    padding: 24px;
+    > .createTag {
+      font-size: 16px;
+      background: $color-shadow;
+      border: none;
+      border: 0.5px solid #404040;
+      border-radius: 5px;
+      padding: 5px 16px;
+    }
   }
 }
 </style>>
-
